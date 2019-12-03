0c17ae7b91eadc182205b35da0e158e00b45da90
Patrick Willner <patrick.willner@tum.de>
2019-12-03 23:16:37 +0100

    Create files for routing

--- src/serial.c
+++ src/serial.c
@@ -1,7 +1,6 @@
 #include "serial.h"
 
 PROCESS(p_serial, "Serial Event listener");
-
 PROCESS_THREAD(p_serial, ev, data)
 {
     PROCESS_BEGIN();
@@ -13,40 +12,60 @@ PROCESS_THREAD(p_serial, ev, data)
 
         if (ev == serial_line_event_message)
         {
-            parse_serial_input((const char*) data);
+            parse_serial_input((const char *)data);
         }
     }
     PROCESS_END();
 }
 
-
-static void parse_serial_input(const char* input)
+/**
+ * @brief Parse a serial input char array
+ * 
+ * @param input 
+ */
+static void parse_serial_input(const char *input)
 {
     printf("Serial Input Received :: %s \r\n", input);
     //format ID:TASK:ARGS
 
-    char* tmp = strtok(input, ":");
+    char *tmp = strtok(input, ":");
     // check id
     int id = atoi(tmp);
 
     tmp = strtok(NULL, ":");
     // check task
-    char* task = tmp;
+    char *task = tmp;
 
     tmp = strtok(NULL, ":");
     //args
-    char* args = tmp;
+    char *args = tmp;
 
     // continue with correct format
     if (strcmp(task, "led") == 0)
     {
         int led_id;
-        if (strcmp(args, "blue") == 0) { led_id=LEDS_BLUE; }
-        else if (strcmp(args, "green") == 0) { led_id=LEDS_GREEN; }
-        else if (strcmp(args, "red") == 0) { led_id=LEDS_RED; }
-        else {led_id=LEDS_ALL; }
+        if (strcmp(args, "blue") == 0)
+        {
+            led_id = LEDS_BLUE;
+        }
+        else if (strcmp(args, "green") == 0)
+        {
+            led_id = LEDS_GREEN;
+        }
+        else if (strcmp(args, "red") == 0)
+        {
+            led_id = LEDS_RED;
+        }
+        else
+        {
+            led_id = LEDS_ALL;
+        }
         leds_toggle(led_id);
     }
-
-
+    else if (strcmp(task, "init") == 0)
+    {
+        // start network discovery
+        leds_off(LEDS_ALL);
+        //init_network();
+    }
 }
